986da1e0a817c0665a8d0c180acd118d77033a5e
AnilSeeroo <anil.raj@seeroo.com>
2024-12-21 00:01:12 +0530

    remove linux package import

--- packages/connectivity_plus/connectivity_plus/lib/connectivity_plus.dart
+++ packages/connectivity_plus/connectivity_plus/lib/connectivity_plus.dart
@@ -11,8 +11,7 @@ import 'package:collection/collection.dart';
 export 'package:connectivity_plus_platform_interface/connectivity_plus_platform_interface.dart'
     show ConnectivityResult;
 
-export 'src/connectivity_plus_linux.dart'
-    if (dart.library.js_interop) 'src/connectivity_plus_web.dart';
+
 
 /// Discover network connectivity configurations: Distinguish between WI-FI and cellular, check WI-FI status and more.
 class Connectivity {
